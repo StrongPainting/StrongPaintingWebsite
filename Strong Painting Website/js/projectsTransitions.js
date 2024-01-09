12d3782b47e134e4dfcae7fc5c9c278d70da6ae3
StrongPainting <156129766+StrongPainting@users.noreply.github.com>
2024-01-09 14:34:55 -0600

    Add files via upload

---
+++ Strong Painting Website/js/projectsTransitions.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    
+    $('.buttons').click(function(){
+
+        $(this).addClass('active').siblings().removeClass('active');
+
+        var filter = $(this).attr('data-filter')
+
+        if (filter == 'all'){
+            $('.image').show(400);
+        } else{
+            $('.image').not('.'+filter).hide(200);
+            $('.image').filter('.'+filter).show(400);
+        }
+    });
+});
